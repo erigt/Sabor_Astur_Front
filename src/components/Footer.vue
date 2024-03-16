fbde5f93f769fb3ff73e8a396663c1e89c626475
Erika <146768635+erigt@users.noreply.github.com>
2024-03-16 09:24:43 +0100

    footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+   <footer>
+    <p>Proyecto personal para el bootcamp de Factoría F5 Full Stack Gijón, realizado por Erika G.</p>
+    <p>Disclaimer: las imágenes han sido tomadas de fuentes anónimas de internet y se desconoce su autoría.</p>
+   </footer>
+  </div>
+  
+</template>
+
+<style scoped lang="scss">
+
+@import url('https://fonts.googleapis.com/css2?family=Hind+Vadodara:wght@300;400;500;600;700&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+
+@media (min-width: 1024px) {
+
+  
+
+
+}
+
+
+
+</style>
